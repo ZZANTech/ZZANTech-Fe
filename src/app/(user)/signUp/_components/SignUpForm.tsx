3a3ba63c15632d4ace36b850d038ca008f12589f
joymet33 <joymet33@gmail.com>
2024-07-22 18:48:51 +0900

    chore: console 삭제, api 자리 주석 남기기

--- src/app/(user)/signUp/_components/SignUpForm.tsx
+++ src/app/(user)/signUp/_components/SignUpForm.tsx
@@ -11,8 +11,15 @@ const SignUpForm = () => {
   const [recheckPassword, setRecheckPassword] = useState("");
   const router = useRouter();
 
+  const isDuplicate = () => {
+    /**
+     * 1. supabase의 회원 목록 조회 : userId
+     * 2. current email과 등일한 이메일이 있는지 확인 : boolean
+     */
+  };
+
   const handleSignUp = () => {
-    console.log("userId/nickname/password/recheckPassword >> ", userId, nickname, password, recheckPassword);
+    // supabase auth signup api
     router.replace("/login");
   };
 
