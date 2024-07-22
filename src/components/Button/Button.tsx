bf65f0127c1cfc048ba235a794bb48f23df943b1
mangmuse <mangmangmuse@gmail.com>
2024-07-22 18:28:13 +0900

    feat: 버튼 컴포넌트 기본세팅

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import Link from "next/link";
+import { ComponentProps, ReactNode } from "react";
 
-function Button() {
-  return <div>Button</div>;
+type ButtonProps =
+  | ({ href?: undefined } & ComponentProps<"button">)
+  | (({ href: string } & ComponentProps<typeof Link>) & {
+      children: ReactNode;
+    });
+
+export default function Button({ children, ...props }: ButtonProps) {
+  const buttonBaseStyle = "px-4 py-2 border border-black";
+  if (props.href) {
+    return (
+      <Link className={buttonBaseStyle} {...props}>
+        {children}
+      </Link>
+    );
+  } else if (typeof props.href === "undefined") {
+    return (
+      <button className={buttonBaseStyle} {...props}>
+        {children}
+      </button>
+    );
+  }
 }
-
-export default Button;
-
-
