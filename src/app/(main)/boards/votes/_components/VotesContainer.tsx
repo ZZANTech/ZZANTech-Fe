528926cf1d6c3e05779367772e188ed4a5bfaf15
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-22 15:55:52 +0900

    feat: add votes list UI

--- src/app/(main)/boards/votes/_components/VotesContainer.tsx
+++ src/app/(main)/boards/votes/_components/VotesContainer.tsx
@@ -1,5 +1,13 @@
+import VotesList from "./VotesList";
+
 function VotesContainer() {
-  return <div>VotesContainer</div>;
+  return (
+    <section>
+      {/* 글쓰기 버튼 */}
+      {/* 정렬 버튼 */}
+      <VotesList />
+    </section>
+  );
 }
 
 export default VotesContainer;
