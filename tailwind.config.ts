60d951ad5d56f1ba13e604af8f5e02881b2b6399
mangmuse <156503764+mangmuse@users.noreply.github.com>
2024-07-26 04:29:35 +0900

    config: tailwind-config 사용할 색상 설정 (#53)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,38 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      },
+      colors: {
+        basic: "#1B1B1B",
+        main: "#E1FF01",
+        point: "#FF6000",
+        ivory: "#F6F5F1",
+
+        accent: {
+          green: "#18A644",
+          purple: "#523BF2",
+          sky: "#52BEFF",
+          pink: "#FF7BBD",
+          "wine-red": "#CE4A38"
+        },
+        info: {
+          yellow: "#FFA000",
+          green: "#04B014",
+          red: "#DC0000"
+        },
+
+        gray: {
+          50: "#F7F7F7",
+          100: "#F6F5F1",
+          200: "#F1F0EB",
+          300: "#E3E3E5",
+          400: "#CCCCC6",
+          500: "#B3B3AE",
+          600: "#8B8B8B",
+          700: "#6C6C6C",
+          800: "#444444",
+          900: "#1B1B1B"
+        }
       }
     }
   },
