d16184a0dea260a62150e29c279236318a3a5325
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-25 22:25:29 +0900

    chore: change placeholders

--- src/app/(main)/boards/votes/_components/VoteWriteForm.tsx
+++ src/app/(main)/boards/votes/_components/VoteWriteForm.tsx
@@ -6,7 +6,12 @@ function VoteWriteForm() {
       <form>
         <div className="flex items-center gap-2">
           <label htmlFor="title">제목</label>
-          <input type="text" id="title" className="mt-1 p-2 border rounded-md" placeholder="제목을 입력하세요" />
+          <input
+            type="text"
+            id="title"
+            className="mt-1 p-2 border rounded-md"
+            placeholder="제목을 입력해 주세요 (2~100자)"
+          />
         </div>
         <div className="flex items-center gap-2">
           <label htmlFor="productName">소비 내역</label>
@@ -14,7 +19,7 @@ function VoteWriteForm() {
             type="text"
             id="productName"
             className="mt-1 p-2 border rounded-md"
-            placeholder="소비 내역을 입력하세요  예) 스탑 워치"
+            placeholder="소비 내역을 입력해 주세요 (2~100자)"
           />
         </div>
         <div className="flex items-center gap-2">
@@ -23,18 +28,22 @@ function VoteWriteForm() {
             type="text"
             id="productPrice"
             className="mt-1 p-2 border rounded-md"
-            placeholder="가격을 입력하세요 예)12000"
+            placeholder="가격을 입력해 주세요 (숫자만, 3~10자)"
           />
         </div>
         <div className="flex items-center gap-2">
           <label htmlFor="content">내용</label>
-          <textarea className="mt-1 p-2 border rounded-md" id="content" placeholder="내용을 입력하세요"></textarea>
+          <textarea
+            className="mt-1 p-2 border rounded-md"
+            id="content"
+            placeholder="내용을 입력해 주세요 (2~200자)"
+          ></textarea>
         </div>
         <div className="flex items-center gap-2">
           <label className="imageUrl">사진 첨부</label>
           <div>
             {/* 첨부파일 미리보기 */}
-            <img src="" alt="" />
+            <img src="" alt="첨부된 사진 미리보기 이미지" />
             <input type="file" id="imageUrl" />
           </div>
         </div>
