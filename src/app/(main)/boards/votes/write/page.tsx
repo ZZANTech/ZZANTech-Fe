10b35478656dc1ad160606fb36e328337307fdd3
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-25 16:43:44 +0900

    feat: add vote creation page ui

---
+++ src/app/(main)/boards/votes/write/page.tsx
@@ -0,0 +1,11 @@
+import VoteWriteForm from "../_components/VoteWriteForm";
+
+function VoteWritePage() {
+  return (
+    <>
+      <VoteWriteForm />
+    </>
+  );
+}
+
+export default VoteWritePage;
