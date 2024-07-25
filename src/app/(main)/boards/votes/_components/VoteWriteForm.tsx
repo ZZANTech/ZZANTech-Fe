448010f79b2f962a00b06431f0be55d4481a7513
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-26 00:38:54 +0900

    feat: post vote

--- src/app/(main)/boards/votes/_components/VoteWriteForm.tsx
+++ src/app/(main)/boards/votes/_components/VoteWriteForm.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import Button from "@/components/Button/Button";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { TVote } from "@/types/vote.type";
+import Button from "@/components/Button/Button";
+import useVoteMutation from "@/stores/queries/useVoteMutation";
+import { useModal } from "@/provider/contexts/modalContext";
 
-function VoteWriteForm() {
+type VoteWriteFormProps = {
+  revalidate?: (voteId: TVote["vote_postId"]) => void;
+};
+
+function VoteWriteForm({ revalidate }: VoteWriteFormProps) {
+  const { addVote } = useVoteMutation(revalidate);
+  const router = useRouter();
+  const modal = useModal();
+
   const [title, setTitle] = useState<string>("");
   const [productName, setProductName] = useState<string>("");
   const [productPrice, setProductPrice] = useState<number>(0);
@@ -17,9 +29,40 @@ function VoteWriteForm() {
     }
   };
 
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    const newVote = {
+      title,
+      product_name: productName,
+      product_price: productPrice,
+      content,
+      image_url: imageUrl,
+      user_id: "a16e76cd-30fb-4130-b321-ec457d17783c"
+    };
+
+    try {
+      await addVote(newVote);
+      // 성공적으로 게시글이 등록되었다는 메시지 표시
+      // 어떻게 보여주는 게 좋을까?
+    } catch (error) {
+      // 오류가 발생하여 게시글 등록에 실패했다는 메시지 표시
+      // 어떻게 보여주는 게 좋을까?
+      console.log(error);
+    }
+  };
+
+  const handleCancel = () => {
+    modal.open({
+      content: "정말 게시글 작성을 취소하시겠습니까?",
+      type: "confirm",
+      onConfirm: () => router.back()
+    });
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex items-center gap-2">
           <label htmlFor="title">제목</label>
           <input
@@ -68,7 +111,9 @@ function VoteWriteForm() {
           </div>
         </div>
         <div>
-          <Button>취소</Button>
+          <Button type="button" onClick={handleCancel}>
+            취소
+          </Button>
           <Button>등록하기</Button>
         </div>
       </form>
