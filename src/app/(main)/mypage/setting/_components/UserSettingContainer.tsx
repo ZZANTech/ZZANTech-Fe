c2cde587fafba79c41055ea7b05423ce7b9b54bd
joymet33 <joymet33@gmail.com>
2024-07-27 01:20:53 +0900

    feat: 마이페이지 회원정보 변경_닉네임 함수 틀 잡기

---
+++ src/app/(main)/mypage/setting/_components/UserSettingContainer.tsx
@@ -0,0 +1,13 @@
+import UserSettingNickname from "@/app/(main)/mypage/setting/_components/UserSettingNickname";
+import UserSettingPassword from "@/app/(main)/mypage/setting/_components/UserSettingPassword";
+
+function UserSettingContainer() {
+  return (
+    <div>
+      <UserSettingNickname />
+      <UserSettingPassword />
+    </div>
+  );
+}
+
+export default UserSettingContainer;
---
+++ src/app/(main)/mypage/setting/_components/UserSettingPassword.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserSettingPassword() {
+  return (
+    <div>UserSettingPassword</div>
+  )
+}
+
+export default UserSettingPassword
