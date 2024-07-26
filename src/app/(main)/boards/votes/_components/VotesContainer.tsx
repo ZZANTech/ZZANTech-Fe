9e071293c95dd4574b9b033790bfe62b3da747b1
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-27 01:12:54 +0900

    feat: user login check and redirect to login page

--- src/app/(main)/boards/votes/_components/VotesContainer.tsx
+++ src/app/(main)/boards/votes/_components/VotesContainer.tsx
@@ -3,17 +3,30 @@
 import useVotesQuery from "@/stores/queries/useVotesQuery";
 import Button from "@/components/Button/Button";
 import VotesList from "@/app/(main)/boards/votes/_components/VotesList";
+import { useUserContext } from "@/provider/contexts/UserContext";
+import { useRouter } from "next/navigation";
 
 function VotesContainer() {
   const { data: votes, isLoading } = useVotesQuery();
+  const { user } = useUserContext();
+  const router = useRouter();
+
+  const handleWriteClick = () => {
+    if (!user) {
+      router.push("/login");
+    } else {
+      router.push("/boards/votes/write");
+    }
+  };
 
   if (isLoading) {
     // 로딩 처리 어떻게 할 것?
     return <div>Loading...</div>;
   }
+
   return (
     <section>
-      <Button href={"/boards/votes/write"}>새 글작성</Button>
+      <Button onClick={handleWriteClick}>글쓰기</Button>
       {/* 정렬 버튼 */}
       <VotesList votes={votes?.data} />
     </section>
