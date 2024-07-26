09ed7721bc0e1013004f7c989567f33398c1341e
NHJeans <guswo1013@naver.com>
2024-07-27 00:14:58 +0900

    feat: home 상단 ui 작업

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import QuizUi from "./_components/QuizUi";
 
 export default function Home() {
   return (
     <main>
-      <h1>짠테크</h1>
-      <QuizUi />
+      <section className="flex flex-col lg:flex-row gap-14">
+        <Image src="/home/home_banner.png" alt="home banner" layout="responsive" width={700} height={250} />
+        <QuizUi />
+      </section>
     </main>
   );
 }
