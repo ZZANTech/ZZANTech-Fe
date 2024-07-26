9caef88b3cc01babe2f323d03aadac5e8c68b9a9
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-26 23:12:04 +0900

    feat: delete vote

--- src/app/(main)/boards/votes/[voteId]/_components/ActionNav.tsx
+++ src/app/(main)/boards/votes/[voteId]/_components/ActionNav.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Button from "@/components/Button/Button";
+import { useModal } from "@/provider/contexts/ModalContext";
+import useVoteMutation from "@/stores/queries/useVoteMutation";
 import { TVote } from "@/types/vote.type";
 
 type ActionNavProps = {
@@ -8,10 +10,24 @@ type ActionNavProps = {
 };
 
 function ActionNav({ vote }: ActionNavProps) {
+  const modal = useModal();
+  const { removeVote } = useVoteMutation();
+
+  const handleDeleteVote = async () => {
+    await removeVote(vote.vote_postId);
+  };
+
+  const handleOpenModal = () =>
+    modal.open({
+      content: "정말 게시글을 삭제하시겠습니까?",
+      type: "confirm",
+      onConfirm: handleDeleteVote
+    });
+
   return (
     <nav className="flex gap-1">
       <Button href={`/boards/votes/edit/${vote.vote_postId}`}>수정</Button>
-      <Button>삭제</Button>
+      <Button onClick={handleOpenModal}>삭제</Button>
       <Button href="/boards/votes">목록으로</Button>
     </nav>
   );
--- src/app/api/votes/[voteId]/route.ts
+++ src/app/api/votes/[voteId]/route.ts
@@ -58,3 +58,25 @@ export const PATCH = async (req: NextRequest, { params }: { params: { voteId: nu
     }
   }
 };
+
+export const DELETE = async (req: NextRequest, { params }: { params: { voteId: number } }) => {
+  const supabase = createClient();
+
+  const voteId = params.voteId;
+  try {
+    if (voteId) {
+      const { status, statusText, error } = await supabase.from("vote_posts").delete().eq("vote_postId", voteId);
+
+      if (error) {
+        throw new Error("게시물 삭제에 실패했습니다");
+      }
+      return NextResponse.json({ status, statusText });
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      return NextResponse.json({ error: e.message }, { status: 500 });
+    } else {
+      return NextResponse.json({ error: "알 수 없는 에러가 발생했습니다" }, { status: 500 });
+    }
+  }
+};
