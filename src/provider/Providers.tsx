10c90b377d212b98f2c758c58ca050f7c4612ff5
joymet33 <joymet33@gmail.com>
2024-07-26 14:39:31 +0900

    config: supabase auth 라이브러리 설치(소셜로그인)

--- src/provider/Providers.tsx
+++ src/provider/Providers.tsx
@@ -1,14 +1,18 @@
 import QueryProvider from "@/provider/QueryProvider";
 import { ModalProvider } from "@/provider/contexts/ModalContext";
 import UserProvider from "@/provider/contexts/UserContext";
+
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { ReactNode } from "react";
 
 function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryProvider>
-      <UserProvider>
-        <ModalProvider>{children}</ModalProvider>
-      </UserProvider>
+      <SessionContextProvider>
+        <UserProvider>
+          <ModalProvider>{children}</ModalProvider>
+        </UserProvider>
+      </SessionContextProvider>
     </QueryProvider>
   );
 }
