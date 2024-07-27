6e595f9e32c90a9dda92657619ae0b19fb967581
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-28 01:32:26 +0900

    feat: display vote percentage and vote count on button click

--- src/app/(main)/boards/votes/[voteId]/_components/VoteButtons.tsx
+++ src/app/(main)/boards/votes/[voteId]/_components/VoteButtons.tsx
@@ -9,9 +9,7 @@ type VoteButtonsProps = {
 
 function VoteButtons({ voteId }: VoteButtonsProps) {
   const { data: counts } = useVoteLikesQuery(voteId);
-  console.log(counts);
-
-  const [isLike, setIsLike] = useState<boolean>(true);
+  const [isLike, setIsLike] = useState<boolean | null>(null);
   const [voteType, setVoteType] = useState<"GOOD" | "BAD" | null>(null);
 
   const handleVote = (type: "GOOD" | "BAD") => {
@@ -21,20 +19,28 @@ function VoteButtons({ voteId }: VoteButtonsProps) {
     }
   };
 
+  const upvotePercentage = counts ? (counts.upvoteCount / counts.totalVoteCount) * 100 : 0;
+  const dounvotePercentage = counts ? (counts.downvoteCount / counts.totalVoteCount) * 100 : 0;
+
+  const upvoteCount = counts ? counts.upvoteCount : 0;
+  const downvoteCount = counts ? counts.downvoteCount : 0;
+
   return (
-    <div className="flex space-x-4">
-      <button
-        className={`px-4 py-2 rounded ${voteType === "GOOD" ? "bg-green-500 text-white" : "bg-gray-800 text-white"}`}
-        onClick={() => handleVote("GOOD")}
-      >
-        {isLike ? "투표 완료" : "👍 GOOD"}
-      </button>
-      <button
-        className={`px-4 py-2 rounded ${voteType === "BAD" ? "bg-green-500 text-white" : "bg-gray-800 text-white"}`}
-        onClick={() => handleVote("BAD")}
-      >
-        {isLike ? "투표 완료" : "👎 BAD"}
-      </button>
+    <div className="flex flex-col space-y-4">
+      <div className="flex space-x-4">
+        <button
+          className={`px-4 py-2 rounded ${voteType === "GOOD" ? "bg-green-500 text-white" : "bg-gray-800 text-white"}`}
+          onClick={() => handleVote("GOOD")}
+        >
+          {voteType === "GOOD" ? `${upvotePercentage}%(${upvoteCount}명)` : "👍 GOOD"}
+        </button>
+        <button
+          className={`px-4 py-2 rounded ${voteType === "BAD" ? "bg-green-500 text-white" : "bg-gray-800 text-white"}`}
+          onClick={() => handleVote("BAD")}
+        >
+          {voteType === "BAD" ? `${dounvotePercentage}%(${downvoteCount}명)` : "👎 BAD"}
+        </button>
+      </div>
     </div>
   );
 }
--- src/stores/queries/useVoteLikesQuery.ts
+++ src/stores/queries/useVoteLikesQuery.ts
@@ -3,7 +3,7 @@ import { TVoteLikeCountsResponse } from "@/types/vote.type";
 import { useQuery } from "@tanstack/react-query";
 
 const useVoteLikesQuery = (voteId: number) => {
-  return useQuery<TVoteLikeCountsResponse[], Error>({
+  return useQuery<TVoteLikeCountsResponse, Error>({
     queryKey: ["voteLikes", { voteId }],
     queryFn: () => getVoteLikesData(voteId)
   });
