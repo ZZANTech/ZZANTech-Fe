a61e52b2ffe838d1b7a3ea83da9d6804afd4ab5e
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-27 18:16:45 +0900

    feat: ui change on vote button click

--- src/app/(main)/boards/votes/[voteId]/_components/VoteButtons.tsx
+++ src/app/(main)/boards/votes/[voteId]/_components/VoteButtons.tsx
@@ -1,11 +1,32 @@
 "use client";
 
+import { useState } from "react";
+
 function VoteButtons() {
+  const [isLike, setIsLike] = useState<boolean>(true);
+  const [voteType, setVoteType] = useState<"GOOD" | "BAD" | null>(null);
+
+  const handleVote = (type: "GOOD" | "BAD") => {
+    if (voteType !== type) {
+      setIsLike(true);
+      setVoteType(type);
+    }
+  };
+
   return (
     <div className="flex space-x-4">
-      {/* 버튼 클릭시 투표 퍼센티지(투표 인원) 표시할 것 */}
-      <button className="bg-gray-800 text-white px-4 py-2 rounded">👍 GOOD</button>
-      <button className="bg-gray-800 text-white px-4 py-2 rounded">👎 BAD</button>
+      <button
+        className={`px-4 py-2 rounded ${voteType === "GOOD" ? "bg-green-500 text-white" : "bg-gray-800 text-white"}`}
+        onClick={() => handleVote("GOOD")}
+      >
+        {isLike ? "투표 완료" : "👍 GOOD"}
+      </button>
+      <button
+        className={`px-4 py-2 rounded ${voteType === "BAD" ? "bg-green-500 text-white" : "bg-gray-800 text-white"}`}
+        onClick={() => handleVote("BAD")}
+      >
+        {isLike ? "투표 완료" : "👎 BAD"}
+      </button>
     </div>
   );
 }
