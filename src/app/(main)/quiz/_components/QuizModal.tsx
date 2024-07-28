84829ea631b61e99c31401fc62e616814d728e18
NHJeans <guswo1013@naver.com>
2024-07-29 00:35:21 +0900

    feat: 퀴즈 문제 출제 UI 작업

--- src/app/(main)/quiz/_components/QuizModal.tsx
+++ src/app/(main)/quiz/_components/QuizModal.tsx
@@ -15,7 +15,7 @@ function QuizModal({ children }: { children: React.ReactNode }) {
       onClick={handleBackdropClick}
       className="fixed left-0 top-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-20 backdrop-blur-sm"
     >
-      <div className="w-[590px] h-[480px] mx-4 md:mx-0 border bg-white rounded-[20px] flex flex-col justify-center items-center">
+      <div className="w-full max-w-[600px] h-[448px] mx-4 md:mx-0 border bg-white rounded-3xl flex flex-col justify-center items-center">
         {children}
       </div>
     </div>
