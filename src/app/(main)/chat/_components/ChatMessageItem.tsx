f93b97563628f83f050d12e328f3471469f5a72c
NHJeans <guswo1013@naver.com>
2024-07-30 03:42:06 +0900

    feat: 동일한 사용자가 일정 시간 동안 채팅을 칠 때 닉네임 하나만 보이도록 구현

--- src/app/(main)/chat/_components/ChatMessageItem.tsx
+++ src/app/(main)/chat/_components/ChatMessageItem.tsx
@@ -4,29 +4,42 @@ import Image from "next/image";
 
 type ChatMessageItemProps = {
   message: TChatWithUser;
+  showNickname: boolean;
 };
 
-function ChatMessageItem({ message }: ChatMessageItemProps) {
+function ChatMessageItem({ message, showNickname }: ChatMessageItemProps) {
   const { user } = useUserContext();
   const isCurrentUser = user?.userId === message.user_id;
 
+  const getDisplayNickname = (nickname: string | undefined) => {
+    if (!nickname) return "";
+    return nickname.length > 12 ? `${nickname.slice(0, 12)}...` : nickname;
+  };
   return (
-    <div className={`flex ${isCurrentUser ? "justify-end" : "justify-start"} mb-2 items-end`}>
-      {isCurrentUser && (
-        <div className="text-xs text-gray-500 mr-2">{new Date(message.created_at).toLocaleTimeString()}</div>
+    <div className={`flex flex-col ${isCurrentUser ? "items-end" : "items-start"} mb-2`}>
+      {!isCurrentUser && showNickname && (
+        <p className="leading-6 text-[#9500DC] mb-1">{getDisplayNickname(message.users?.nickname)}</p>
       )}
-      <div
-        className={`max-w-xs px-4 py-2 rounded-lg ${isCurrentUser ? "bg-main text-black" : "bg-[#EEE2FA] text-black"} relative`}
-      >
-        <div className={`text-sm ${isCurrentUser ? "text-right" : "text-left"}`}>
-          {!isCurrentUser && <p className="text-purple-500">{message.users?.nickname}</p>}
-          {message.image_url && <Image src={message.image_url} alt="chat image" width={100} height={100} />}
-          <p className="break-words">{message.content}</p>
+      <div className={`flex ${isCurrentUser ? "justify-end" : "justify-start"} items-end`}>
+        {isCurrentUser && (
+          <div className="text-xs text-gray-500 mr-2">{new Date(message.created_at).toLocaleTimeString()}</div>
+        )}
+        <div
+          className={`max-w-xs px-5 py-2 flex items-center gap-2 ${
+            isCurrentUser
+              ? "bg-main rounded-bl-[28px] rounded-br-[28px] rounded-tl-[28px]"
+              : "bg-[#EEE2FA] rounded-br-[28px] rounded-bl-[28px] rounded-tr-[28px]"
+          } relative`}
+        >
+          <div className={`text-sm ${isCurrentUser ? "text-right" : "text-left"}`}>
+            {message.image_url && <Image src={message.image_url} alt="chat image" width={100} height={100} />}
+            <p className="break-all">{message.content}</p>
+          </div>
         </div>
+        {!isCurrentUser && (
+          <div className="text-xs text-gray-500 ml-2">{new Date(message.created_at).toLocaleTimeString()}</div>
+        )}
       </div>
-      {!isCurrentUser && (
-        <div className="text-xs text-gray-500 ml-2">{new Date(message.created_at).toLocaleTimeString()}</div>
-      )}
     </div>
   );
 }
--- src/app/(main)/chat/_components/ChatMessageList.tsx
+++ src/app/(main)/chat/_components/ChatMessageList.tsx
@@ -1,22 +1,34 @@
 "use client";
 
 import ChatMessageItem from "@/app/(main)/chat/_components/ChatMessageItem";
+import LoadingSpinner from "@/components/Loading/LoadingSpinner";
 import { useChatMessagesQuery } from "@/stores/queries/useChatMessagesQuery";
 import { TChatWithUser } from "@/types/chat.type";
 
 type ChatMessageListProps = {
   roomId: number;
 };
+const TIME_THRESHOLD = 60 * 1000;
 
 function ChatMessageList({ roomId }: ChatMessageListProps) {
   const { data: chats, isPending } = useChatMessagesQuery(roomId);
 
   if (isPending) {
-    return <div>loading</div>;
+    return <LoadingSpinner />;
   }
 
   return (
-    <div>{chats?.map((message: TChatWithUser) => <ChatMessageItem key={message.chatId} message={message} />)}</div>
+    <div>
+      {chats?.map((message: TChatWithUser, index: number) => {
+        const prevMessage = chats[index - 1];
+        const showNickname =
+          !prevMessage ||
+          prevMessage.user_id !== message.user_id ||
+          new Date(message.created_at).getTime() - new Date(prevMessage.created_at).getTime() > TIME_THRESHOLD;
+
+        return <ChatMessageItem key={message.chatId} message={message} showNickname={showNickname} />;
+      })}
+    </div>
   );
 }
 
