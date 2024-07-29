aa32cc0a324676589275e3ae1ea80d10a30c0495
NHJeans <guswo1013@naver.com>
2024-07-30 02:38:48 +0900

    feat: 채팅방 메세지 및 채팅 입력창 UI 작업

--- src/app/(main)/chat/_components/ChatMessageContainer.tsx
+++ src/app/(main)/chat/_components/ChatMessageContainer.tsx
@@ -9,12 +9,15 @@ function ChatMessageContainer({ roomId }: { roomId: number }) {
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="flex flex-col h-[60vh] bg-white shadow-lg rounded-lg overflow-hidden">
-        <div className="flex-grow overflow-y-auto p-4">
-          <ChatMessageList roomId={roomId} />
-        </div>
-        <div className="p-4 bg-gray-200">
-          <ChatMessageInput roomId={roomId} />
+      <div>
+        <p>지금 25명의 짠테커들이 함께 이야기를 나누고 있어요!</p>
+        <div className="flex flex-col h-[60vh] bg-white shadow-lg rounded-lg overflow-hidden">
+          <div className="flex-grow overflow-y-auto p-4">
+            <ChatMessageList roomId={roomId} />
+          </div>
+          <div className="p-4 bg-gray-200">
+            <ChatMessageInput roomId={roomId} />
+          </div>
         </div>
       </div>
     </div>
--- src/app/(main)/chat/_components/ChatMessageItem.tsx
+++ src/app/(main)/chat/_components/ChatMessageItem.tsx
@@ -11,17 +11,22 @@ function ChatMessageItem({ message }: ChatMessageItemProps) {
   const isCurrentUser = user?.userId === message.user_id;
 
   return (
-    <div className={`flex ${isCurrentUser ? "justify-end" : "justify-start"} mb-2`}>
+    <div className={`flex ${isCurrentUser ? "justify-end" : "justify-start"} mb-2 items-end`}>
+      {isCurrentUser && (
+        <div className="text-xs text-gray-500 mr-2">{new Date(message.created_at).toLocaleTimeString()}</div>
+      )}
       <div
-        className={`max-w-xs px-4 py-2 rounded-lg ${isCurrentUser ? "bg-[#E1FF01] text-black" : "bg-purple-100 text-black"}`}
+        className={`max-w-xs px-4 py-2 rounded-lg ${isCurrentUser ? "bg-main text-black" : "bg-[#EEE2FA] text-black"} relative`}
       >
         <div className={`text-sm ${isCurrentUser ? "text-right" : "text-left"}`}>
           {!isCurrentUser && <p className="text-purple-500">{message.users?.nickname}</p>}
-          {message.image_url && <Image src={message.image_url} alt="chat image" width={200} height={200} />}
-          <p>{message.content}</p>
-          <p className="text-xs text-gray-500">{new Date(message.created_at).toLocaleTimeString()}</p>
+          {message.image_url && <Image src={message.image_url} alt="chat image" width={100} height={100} />}
+          <p className="break-words">{message.content}</p>
         </div>
       </div>
+      {!isCurrentUser && (
+        <div className="text-xs text-gray-500 ml-2">{new Date(message.created_at).toLocaleTimeString()}</div>
+      )}
     </div>
   );
 }
