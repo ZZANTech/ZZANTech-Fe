dccf9c496f602ba041992bc5fe6feef99364c90f
mangmuse <156503764+mangmuse@users.noreply.github.com>
2024-07-29 09:32:31 +0900

    feat: users 테이블에 badge_url 추가 및 route handler 수정 (#105)
    * fix: get knowhow route handler
    
    * refactor: remove unused console.log
    
    * refactor: remove unused console.log

--- src/app/(main)/boards/knowhow/_components/knowhowItem/KnowhowItem.tsx
+++ src/app/(main)/boards/knowhow/_components/knowhowItem/KnowhowItem.tsx
@@ -19,7 +19,6 @@ function KnowhowItem({ knowhow }: KnowhowItemProps) {
   const { title, content, nickname, created_at, comments_count, likes_count, image_urls, knowhow_postId } = knowhow;
   const formattedCreatedAt = dayjs(created_at).fromNow();
   const textOnlyContent = content.replace(/<[^>]+>/g, "");
-  console.log(textOnlyContent);
 
   return (
     <li className="w-full h-[220px] border   border-[#111] rounded-xl px-10 py-5">
@@ -27,7 +26,10 @@ function KnowhowItem({ knowhow }: KnowhowItemProps) {
         <div className="w-full   flex gap-9 relative">
           <div className="flex w-full   flex-col overflow-hidden">
             <div className="flex gap-3 items-center mb-3.5">
-              <span className="font-semibold">{nickname}</span>
+              <div className="flex gap-2">
+                <Image src={knowhow.badge_url || ""} alt="badge" width={24} height={24} />
+                <span className="font-semibold">{nickname}</span>
+              </div>
               <time className="text-sm text-[#ADADAD]">{formattedCreatedAt}</time>
             </div>
             <h4 className="text-xl font-semibold text-[#000] mb-3.5 truncate">{title}</h4>
