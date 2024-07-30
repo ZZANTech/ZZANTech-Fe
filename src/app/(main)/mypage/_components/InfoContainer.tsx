b6f0c107c8f88b9575215af7c1b401d2b5894e1a
joymet33 <joymet33@gmail.com>
2024-07-30 23:55:01 +0900

    design: 마이페이지 디자인 개선

--- src/app/(main)/mypage/_components/InfoContainer.tsx
+++ src/app/(main)/mypage/_components/InfoContainer.tsx
@@ -9,18 +9,18 @@ function InfoContainer() {
   const { user } = useUserContext();
   // console.log("user", user);
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-row gap-2.5">
-        <Image src={user?.badge_url || ""} width={24} height={24} alt="mainLogo" />
-        <p>{user?.nickname}님</p>
+    <div className="flex flex-row justify-between">
+      <div>
+        <div className="flex flex-row gap-2.5">
+          <Image src={user?.badge_url || ""} width={24} height={24} alt="mainLogo" />
+          <p>{user?.nickname}님</p>
+        </div>
+        <div className="flex flex-row gap-2.5">
+          <p>이메일 주소</p>
+          <p>{user?.email}</p>
+        </div>
       </div>
-
-      <div className="flex flex-row gap-2.5">
-        <p>이메일 주소</p>
-        <p>{user?.email}</p>
-      </div>
-
-      <Link href={`${BASE_URL}/mypage/edit`} className="bg-green-100">
+      <Link href={`${BASE_URL}/mypage/edit`} className="bg-black text-white rounded-lg">
         회원 정보 변경
       </Link>
     </div>
