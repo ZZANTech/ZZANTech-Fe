996a9fc77c9f646a64e6028ac05fc929bab41c6c
NHJeans <guswo1013@naver.com>
2024-07-31 02:36:53 +0900

    feat: 이메일 및 닉네임 유효성 검사 및 중복 확인 결과에 따른 스타일링 추가
    - 이메일 유효성 검사
    - 이메일 중복 확인 결과에 따른 경고 및 성공 메시지 스타일링 추가 (info-red, info-green)
    - 닉네임 형식 유효성 검사 추가 (영문, 숫자, 한글 3자 이상 20자 이하)
    - 올바르지 않은 닉네임 형식에 대한 경고 메시지 추가
    - 닉네임 중복 확인 결과에 따른 경고 및 성공 메시지 스타일링 추가 (info-red, info-green)
    - 유효성 검사 및 중복 체크 상태를 관리하는 로직 추가

--- src/app/(auth)/signUp/_components/NicknameForm.tsx
+++ src/app/(auth)/signUp/_components/NicknameForm.tsx
@@ -4,11 +4,23 @@ import { createClient } from "@/utils/supabase/client";
 import { ChangeEvent, MouseEventHandler, useState } from "react";
 
 function NicknameForm({ nickname, setNickname }: { nickname: string; setNickname: (nickname: string) => void }) {
-  const [isDuplicated, setIsDuplicated] = useState(false);
-  const [isCorrected, setIsCorrected] = useState(false);
+  const [isDuplicated, setIsDuplicated] = useState<boolean>(false);
+  const [isCorrected, setIsCorrected] = useState<boolean>(false);
+  const [isInvalidNickname, setIsInvalidNickname] = useState<boolean>(false);
   const supabase = createClient();
 
   const handleCheckDuplicate: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    setIsInvalidNickname(false);
+
+    const nicknameRegex = /^[a-zA-Z0-9가-힣]{3,20}$/;
+    if (!nicknameRegex.test(nickname)) {
+      setIsInvalidNickname(true);
+      setIsCorrected(false);
+      setIsDuplicated(false);
+      return;
+    }
+
     e.preventDefault();
     let { data: users, error } = await supabase.from("users").select("*").eq("nickname", nickname);
     if (users!.length > 0) {
@@ -29,17 +41,23 @@ function NicknameForm({ nickname, setNickname }: { nickname: string; setNickname
           type="text"
           value={nickname}
           placeholder="닉네임을 입력해주세요"
-          className="AuthInputShort"
+          className={`AuthInputShort ${
+            isDuplicated || isInvalidNickname ? "border-info-red" : isCorrected ? "border-info-green" : ""
+          }`}
           onChange={(e) => {
             setNickname(e.target.value);
+            setIsDuplicated(false);
+            setIsCorrected(false);
+            setIsInvalidNickname(false);
           }}
         />
         <button className="AuthDupButton" onClick={handleCheckDuplicate}>
           중복체크
         </button>
       </form>
-      {isDuplicated ? <p className="AuthStateInfo">동일한 닉네임이 있습니다.</p> : ""}
-      {isCorrected ? <p className="AuthStateInfoGreen">사용 가능한 닉네임입니다.</p> : ""}
+      {isDuplicated && <p className="AuthStateInfo">동일한 닉네임이 있습니다.</p>}
+      {isCorrected && <p className="AuthStateInfoGreen">사용 가능한 닉네임입니다.</p>}
+      {isInvalidNickname && <p className="AuthStateInfo text-info-red">유효한 닉네임 형식이 아닙니다.</p>}
     </div>
   );
 }
