e638dcef1705694af050dc4db0bc30ae2512d073
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-08-01 19:06:15 +0900

    feat: update nav button ui using image files

--- src/app/(main)/boards/votes/[voteId]/_components/NavButton.tsx
+++ src/app/(main)/boards/votes/[voteId]/_components/NavButton.tsx
@@ -2,6 +2,8 @@
 
 import { TVoteWithNavigation } from "@/types/vote.type";
 import { useRouter, useSearchParams } from "next/navigation";
+import Image from "next/image";
+import { useState } from "react";
 
 type NavButtonProps = {
   vote: TVoteWithNavigation;
@@ -12,8 +14,8 @@ function NavButton({ vote, direction }: NavButtonProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
   const sortOrder = searchParams.get("sortOrder") || "latest";
-
   const targetVoteId = direction === "next" ? vote.nextVoteId : vote.prevVoteId;
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleClick = () => {
     if (targetVoteId) {
@@ -21,19 +23,22 @@ function NavButton({ vote, direction }: NavButtonProps) {
     }
   };
 
+  const getImageSrc = () => {
+    const directionStr = direction === "next" ? "right" : "left";
+    const state = !targetVoteId ? "nonactive" : isHovered ? "hover" : "Default";
+
+    return `/icons/vote/direction=${directionStr}, state=${state}.png`;
+  };
+
   return (
     <button
-      className={`w-[74px] h-[74px] relative rounded-[74px] border border-[#1b1b1b] ${
-        targetVoteId ? "bg-white" : "bg-gray-300"
-      }`}
+      className="w-[74px] h-[74px] relative rounded-[74px] border border-[#1b1b1b] bg-white"
       onClick={handleClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       disabled={!targetVoteId}
     >
-      {direction === "next" ? (
-        <div className="w-[15px] h-[15px] left-[35px] top-[26.39px] absolute origin-top-left rotate-45 border-r border-t border-[#1b1b1b]" />
-      ) : (
-        <div className="w-[15px] h-[15px] left-[39px] top-[47.61px] absolute origin-top-left rotate-[-135deg] border-r border-t border-[#1b1b1b]" />
-      )}
+      <Image src={getImageSrc()} alt={`${direction} navigation`} width={74} height={74} className="object-contain" />
     </button>
   );
 }
