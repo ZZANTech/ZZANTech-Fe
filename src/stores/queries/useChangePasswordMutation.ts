e77b16164276504b70c59c85e27c7d1b9c9c0e97
mangmuse <mangmangmuse@gmail.com>
2024-08-02 23:29:18 +0900

    feat: password-modal

--- src/stores/queries/useChangePasswordMutation.ts
+++ src/stores/queries/useChangePasswordMutation.ts
@@ -1,15 +1,29 @@
 import { patchPassword } from "@/apis/auth";
 import useAlertModal from "@/hooks/useAlertModal";
+import { useModal } from "@/provider/contexts/ModalContext";
 import { TChangePassword } from "@/types/user.type";
+import { splitMessage } from "@/utils/splitMessage";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 const useChangePasswordMutation = () => {
+  const router = useRouter();
+  const { open } = useModal();
   const { displayDefaultAlert } = useAlertModal();
   const { mutateAsync: updatePassword } = useMutation<string, { status: number; message: string }, TChangePassword>({
     mutationFn: (password) => patchPassword(password),
-    onSuccess: (successMessage) => displayDefaultAlert(successMessage),
+    onSuccess: (successMessage) => {
+      open({
+        type: "alert",
+        content: successMessage,
+        onClose: () => router.replace("/mypage")
+      });
+    },
 
-    onError: (e) => displayDefaultAlert(e.message)
+    onError: (e) => {
+      const { firstLine, secondLine } = splitMessage(e.message);
+      displayDefaultAlert(firstLine, secondLine);
+    }
   });
   return { updatePassword };
 };
