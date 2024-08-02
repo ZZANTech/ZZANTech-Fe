6bfd7a582d5a738d3f94a45f928a8ea5d6ecde59
NHJeans <guswo1013@naver.com>
2024-08-03 01:53:18 +0900

    design: 베스트 짠 노하우 배경 추가

--- src/app/(main)/_components/LeftSection/TopKnowhows.tsx
+++ src/app/(main)/_components/LeftSection/TopKnowhows.tsx
@@ -1,20 +1,29 @@
 "use client";
 
 import useTopKnowhowQuery from "@/stores/queries/useTopKnowhowQuery";
+import Image from "next/image";
 import Link from "next/link";
 
 function TopKnowhows() {
   const { data } = useTopKnowhowQuery();
 
+  const backgrounds = ["/home/Knowhowbg1.png", "/home/Knowhowbg2.png", "/home/Knowhowbg3.png"];
+
   return (
     <div>
-      <h3 className="text-xl mb-4">Best 짠노하우 👑</h3>
+      <h3 className="text-xl mb-4">Best 짠노하우 💎</h3>
       <ul className="space-y-4">
-        {data?.map((knowhow) => (
+        {data?.map((knowhow, index) => (
           <li key={knowhow.knowhow_postId} className="my-2">
             <Link href={`/boards/knowhow/${knowhow.knowhow_postId}`}>
-              <div className="bg-gray-700 rounded-xl">
-                <div className="pt-20 pl-5">
+              <div className="relative rounded-xl overflow-hidden h-40">
+                <Image
+                  src={backgrounds[index % backgrounds.length]}
+                  alt={`Background ${index + 1}`}
+                  layout="fill"
+                  className="rounded-xl"
+                />
+                <div className="absolute bottom-0 left-0 pl-5 w-full">
                   <h6 className="text-lg text-white font-semibold">{knowhow.title}</h6>
                   <p className="text-white pb-5">{knowhow.nickname}</p>
                 </div>
