c293685bc2447201303280b74496556d1b88e4e2
NHJeans <guswo1013@naver.com>
2024-08-03 00:26:40 +0900

    design: top section 레이아웃 수정

--- src/app/(main)/_components/QuizUi.tsx
+++ src/app/(main)/_components/QuizUi.tsx
@@ -8,19 +8,18 @@ const QuizUi = () => {
   const { hasTakenQuiz, isLoading } = useUserContext();
 
   return (
-    <div className="w-[360px] h-[250px] bg-gray-900 rounded-[20px] p-6 flex flex-col justify-between items-center">
+    <div className="w-full h-[250px] bg-gray-900 rounded-[20px] px-6 py-7 flex flex-col justify-between items-center">
       <section className="w-full flex justify-between items-center px-1">
-        <div className="text-main w-[180px]">
+        <div className="text-main">
           {isLoading ? (
-            <div className=" w-[180px]">
-              <div className="animate-pulse w-[160px] h-[25px] bg-gray-700 rounded-md mb-2 mr-1"></div>
-              <div className="animate-pulse w-[160px] h-[25px] bg-gray-700 rounded-md mb-2 mr-1"></div>
-              <div className="animate-pulse w-[160px] h-[25px] bg-gray-700 rounded-md mb-2 mr-1"></div>
+            <div>
+              <div className="animate-pulse w-[140px] h-[25px] bg-gray-700 rounded-md mb-2 mr-1"></div>
+              <div className="animate-pulse w-[140px] h-[25px] bg-gray-700 rounded-md mb-2 mr-1"></div>
             </div>
           ) : (
-            <div className="w-[180px]">
-              <p className="text-[20px]">{hasTakenQuiz ? "이미 오늘의 퀴즈를" : "오늘의 퀴즈 풀고"}</p>
-              <p className="text-[20px]">{hasTakenQuiz ? "풀었어요!" : "포인트 받자!"}</p>
+            <div>
+              <p className="text-xl">{hasTakenQuiz ? "이미 오늘의 퀴즈를" : "오늘의 퀴즈 풀고"}</p>
+              <p className="text-xl">{hasTakenQuiz ? "풀었어요!" : "포인트 받자!"}</p>
             </div>
           )}
         </div>
@@ -38,7 +37,7 @@ const QuizUi = () => {
         {isLoading ? (
           <div></div>
         ) : hasTakenQuiz ? (
-          <button className="w-[312px] h-[56px] bg-gray-100 font-semibold rounded-md" disabled>
+          <button className="w-[312px] h-[56px] font-semibold text-[#999999] rounded-md" disabled>
             내일 또 참여해 주세요!
           </button>
         ) : (
