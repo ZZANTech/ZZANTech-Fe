7ab752a20dcffd7aaa8fc5d7a9096209c4bd7817
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-08-03 01:14:53 +0900

    fix: build error

--- src/app/(main)/mypage/point/page.tsx
+++ src/app/(main)/mypage/point/page.tsx
@@ -1,11 +1,14 @@
 import MyCurrentPoints from "@/app/(main)/mypage/_components/Points/MyCurrentPoints";
 import MyPointsHistoryTable from "@/app/(main)/mypage/_components/Points/MyPointsHistoryTable";
+import { Suspense } from "react";
 
 function MyPointDetailPage() {
   return (
     <section className="w-[933.08px] flex flex-col items-center justify-center gap-16 mx-auto">
       <MyCurrentPoints />
-      <MyPointsHistoryTable />
+      <Suspense>
+        <MyPointsHistoryTable />
+      </Suspense>
     </section>
   );
 }
