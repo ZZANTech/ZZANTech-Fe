8934836c0c1b4fd610fc3b622dd31d94f9324f4f
mangmuse <mangmangmuse@gmail.com>
2024-08-03 18:55:52 +0900

    design: knowhow list skeleton ui

--- src/app/(main)/boards/knowhow/_components/KnowhowContainer.tsx
+++ src/app/(main)/boards/knowhow/_components/KnowhowContainer.tsx
@@ -10,25 +10,12 @@ import {
   SORT_OPTIONS,
   TOption
 } from "@/app/(main)/boards/knowhow/_constants";
-import dynamic from "next/dynamic";
 import SearchOptions from "@/app/(main)/boards/knowhow/_components/SearchOptions";
 import { useRouter, useSearchParams } from "next/navigation";
-import Button from "@/components/Button/Button";
 import { useModal } from "@/provider/contexts/ModalContext";
 import useAlertModal from "@/hooks/useAlertModal";
 import KnowhowList from "@/app/(main)/boards/knowhow/_components/KnowhowList";
-
-// const KnowhowList = dynamic(() => import("@/app/(main)/boards/knowhow/_components/KnowhowList"), {
-//   loading: () => (
-//     <ul className="flex flex-col  gap-8 mb-[13px]">
-//       <li className="w-full h-[220px] bg-gray-50 rounded-xl px-10 py-5"></li>
-//       <li className="w-full h-[220px] bg-gray-50 rounded-xl px-10 py-5"></li>
-//       <li className="w-full h-[220px] bg-gray-50 rounded-xl px-10 py-5"></li>
-//       <li className="w-full h-[220px] bg-gray-50 rounded-xl px-10 py-5"></li>
-//     </ul>
-//   ),
-//   ssr: false
-// });
+import SkeletonKnowhowList from "@/app/(main)/boards/knowhow/_components/SkeletonKnowhowList";
 
 function KnowhowContainer() {
   const { displayDefaultAlert } = useAlertModal();
@@ -40,7 +27,7 @@ function KnowhowContainer() {
   const searchParams = useSearchParams();
   const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const { data: knowhows } = useKnowhowsQuery(
+  const { data: knowhows, isPending } = useKnowhowsQuery(
     currentPage,
     ITEMS_PER_PAGE,
     sortOrder,
@@ -91,7 +78,7 @@ function KnowhowContainer() {
         onSearch={handleSearch}
         sortOrder={sortOrder}
       />
-      <KnowhowList knowhows={knowhows?.posts} />
+      {isPending ? <SkeletonKnowhowList /> : <KnowhowList knowhows={knowhows?.posts} />}
       <div className="flex flex-col self-center relative">
         <Suspense>
           <Pagination itemsPerPage={ITEMS_PER_PAGE} totalItems={totalItems || 0} onPageChange={handlePageChange} />
