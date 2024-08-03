a530e1d68b8307f2de9b94e55b7564b6db663488
joymet33 <joymet33@gmail.com>
2024-08-04 08:04:15 +0900

    design: 포인트의 천단위 표시

--- src/app/(main)/mypage/_components/PointContainer.tsx
+++ src/app/(main)/mypage/_components/PointContainer.tsx
@@ -8,6 +8,8 @@ import { useRouter } from "next/navigation";
 function PointContainer() {
   const { user } = useUserContext();
   const router = useRouter();
+  const total_point = user?.total_point.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const current_point = user?.current_point.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   const handlePointsHistoryClick = () => {
     router.push("/mypage/point");
@@ -21,12 +23,12 @@ function PointContainer() {
             <p className="font-bold text-point leading-5">사용 가능 포인트</p>
             <div className="flex gap-2 items-center">
               <Image src={"/icons/mypage/coin.png"} width={36} height={36} alt="diamond" />
-              <p className="text-2xl font-bold">{user?.current_point} P</p>
+              <p className="text-2xl font-bold">{current_point} P</p>
             </div>
           </div>
           <div className="flex flex-col gap-3">
             <p className="font-bold leading-5">누적 포인트</p>
-            <p className="text-2xl font-bold text-gray-800">{user?.total_point} P</p>
+            <p className="text-2xl font-bold text-gray-800">{total_point} P</p>
           </div>
         </div>
 
@@ -38,7 +40,7 @@ function PointContainer() {
         </button>
       </div>
       <div className="flex justify-between">
-        <p className="text-sm">포인트 3000점이 넘으면 기프티콘으로 교환하실 수 있어요</p>
+        <p className="text-sm">포인트 3,000점이 넘으면 기프티콘으로 교환하실 수 있어요</p>
         <Link href={"/exchange"} className="text-sm font-semibold underline">
           교환하기
         </Link>
