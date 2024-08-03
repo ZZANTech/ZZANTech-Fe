09deaf9af8359d16a51b15b0b482cdbe21ec314f
NHJeans <guswo1013@naver.com>
2024-08-03 22:05:51 +0900

    design: 채팅방 리스트 주의사항 문구 추가

--- src/app/(main)/chat/page.tsx
+++ src/app/(main)/chat/page.tsx
@@ -7,6 +7,13 @@ function ChatPage() {
       <p className="text-xl">살까 말까 고민되는 상품을 올리면 실시간으로 고민을 해결해주는 라이브 채팅방입니다!</p>
       <p className="text-xl mb-8">우리 다같이 똑똑한 소비자가 되어 보자구 🔎</p>
       <ChatRoomList />
+      <div className="bg-ivory max-w-[660px] p-5 mt-8 mx-auto rounded-md border">
+        <strong className="text-info-red text-xl pl-2">주의 사항</strong>
+        <p className="text-gray-900 mt-3">
+          • 개인정보를 함부로 유출하지 않도록 주의해야 합니다.
+          <br />• 비방 및 욕설 금지: 다른 사용자를 비방하거나 욕설하는 행위는 금지됩니다.
+        </p>
+      </div>
     </div>
   );
 }
