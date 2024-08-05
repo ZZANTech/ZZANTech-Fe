78ae52e2b6f680b4be3d6fcf02534959486b1e75
mangmuse <156503764+mangmuse@users.noreply.github.com>
2024-08-05 10:41:33 +0900

    feat: 간단한 메타데이터 설정 / design: 짠 노하우 페이지 search 바 아웃라인 제거 (#212)
    * design: remove search input outline
    
    * feat: set metadata for SEO

--- src/app/(main)/exchange/page.tsx
+++ src/app/(main)/exchange/page.tsx
@@ -1,6 +1,13 @@
 import { getGifts } from "@/apis/exchange";
 import ExchangeContainer from "@/app/(main)/exchange/_components/ExchangeContainer";
 import { HydrationBoundary, QueryClient, dehydrate, queryOptions } from "@tanstack/react-query";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ZZAN | 포인트샵",
+  description:
+    "모은 포인트로 다양한 기프티콘을 교환해 보세요! 포인트를 활용해 즐거운 소비를 경험할 수 있는 곳입니다. 함께 스마트한 소비를 실현해봐요!"
+};
 
 async function ExchangePage() {
   const queryClient = new QueryClient({});
