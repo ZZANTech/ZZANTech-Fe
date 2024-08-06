28fd349073cb2d5064dcb87f7a5ed4b61a32c817
mangmuse <156503764+mangmuse@users.noreply.github.com>
2024-08-06 16:43:35 +0900

    fix: 게시글 작성, 수정, 기프티콘 구매시 중복요청 방지 (#226)
    * feat: prevent duplicate submission and display loading spinner
    
    * feat: prevent duplicate submission and display loading spinner on gift page
    
    * fix: prevent duplicate submission
    
    * fix: resolve build error

--- src/components/Loading/LoadingSpinner.tsx
+++ src/components/Loading/LoadingSpinner.tsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
 import React from "react";
+import clsx from "clsx";
 
-function LoadingSpinner() {
+type LoadingSpinnerProps = {
+  isSubmitting?: boolean;
+};
+
+function LoadingSpinner({ isSubmitting = false }: LoadingSpinnerProps) {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="relative w-28 h-28">
-        <div className="absolute inset-0 border-[15px] border-orange-200 rounded-full" />
-        <Image src="/icons/Spiner.svg" alt="Loading Spinner" className="animate-spin" width={112} height={112} />
+    <div
+      className={clsx("fixed inset-0 z-50 flex items-center justify-center", {
+        "bg-black bg-opacity-20 pointer-events-auto select-none": isSubmitting,
+        "pointer-events-none": !isSubmitting
+      })}
+    >
+      <div className="relative w-28 h-28 pointer-events-none">
+        <div className="absolute inset-0 border-[15px] border-orange-200 rounded-full pointer-events-none" />
+        <Image
+          src="/icons/Spiner.svg"
+          alt="Loading Spinner"
+          className="animate-spin pointer-events-none"
+          width={112}
+          height={112}
+        />
       </div>
     </div>
   );
