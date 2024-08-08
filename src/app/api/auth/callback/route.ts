cda572325b368296711cbcb099303371870a7a4b
joymet33 <joymet33@gmail.com>
2024-08-08 18:59:27 +0900

    chore: 콘솔 위치 변경

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -5,11 +5,12 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/";
-  console.log("faslkdjfnaskdjfnalskdjfnlsdjksdflkgjsdfkljghnskldjfgnlsdkjfg", code);
+  console.log("보여줘라 >>>>>>>>>>>>>>>>>>>>>", code);
 
   if (code) {
     const supabase = createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host");
       const isLocalEnv = process.env.NODE_ENV === "development";
