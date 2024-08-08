ffa881da14aa032cc24011d09048b025724d2841
NHJeans <guswo1013@naver.com>
2024-08-08 17:26:06 +0900

    feat: 메인페이지 포인트 랭킹 스켈레톤UI 적용

--- src/app/(main)/_components/LeftSection/SkeletonVotes.tsx
+++ src/app/(main)/_components/LeftSection/SkeletonVotes.tsx
@@ -3,12 +3,12 @@ import React from "react";
 function SkeletonVotes() {
   return (
     <div className="animate-pulse flex flex-col h-full">
-      <div className="relative w-full pb-[75%] bg-gray-300 rounded-xl"></div>
+      <div className="relative w-full pb-[75%] bg-gray-300 rounded-xl" />
       <div className="flex justify-between mt-2">
-        <div className="w-1/2 h-4 bg-gray-300 rounded"></div>
+        <div className="w-1/2 h-4 bg-gray-300 rounded" />
         <div className="flex items-center">
-          <div className="w-5 h-5 bg-gray-300 rounded-full"></div>
-          <div className="w-10 h-4 bg-gray-300 rounded ml-3"></div>
+          <div className="w-5 h-5 bg-gray-300 rounded-full" />
+          <div className="w-4 h-4 bg-gray-300 rounded ml-2" />
         </div>
       </div>
     </div>
--- src/app/(main)/_components/RightSection/PointRankingList.tsx
+++ src/app/(main)/_components/RightSection/PointRankingList.tsx
@@ -1,9 +1,20 @@
 "use client";
 import PointRankingItem from "@/app/(main)/_components/RightSection/PointRankingItem";
+import SkeletonPointRankingItem from "@/app/(main)/_components/RightSection/SkeletonPointRankingItem";
 import usePointRankingsQuery from "@/stores/queries/usePointRankingsQuery";
 
 function PointRankingList() {
-  const { data: rankings } = usePointRankingsQuery();
+  const { data: rankings, isPending } = usePointRankingsQuery();
+
+  if (isPending) {
+    return (
+      <ul className="space-y-3">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <SkeletonPointRankingItem key={index} />
+        ))}
+      </ul>
+    );
+  }
   return (
     <ul className="space-y-3">
       {rankings?.map((ranking, index) => <PointRankingItem key={ranking.userId} index={index} ranking={ranking} />)}
