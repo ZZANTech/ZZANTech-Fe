25eaf5694eb56c9768e575aa11ee1e9c295644e9
NHJeans <guswo1013@naver.com>
2024-08-12 02:50:58 +0900

    design: 채팅방 디자인 수정

--- src/app/(main)/chat/_components/ChatMessageContainer.tsx
+++ src/app/(main)/chat/_components/ChatMessageContainer.tsx
@@ -3,15 +3,16 @@
 import ChatMessageInput from "@/app/(main)/chat/_components/ChatMessageInput";
 import ChatMessageList from "@/app/(main)/chat/_components/ChatMessageList";
 import { useRealtimeChats } from "@/app/(main)/chat/_utils/useRealtimeChats";
+import Image from "next/image";
 
 function ChatMessageContainer({ roomId }: { roomId: number }) {
   useRealtimeChats(roomId);
 
   return (
-    <div className="flex items-center justify-center  bg-gray-100 h-dvh ">
+    <div className="flex items-center justify-center h-dvh">
       <div>
-        <p>지금 25명의 짠테커들이 함께 이야기를 나누고 있어요!</p>
-        <div className="flex flex-col h-[60vh] bg-white shadow-lg rounded-lg overflow-hidden">
+        <Image src="/icons/chat/chatRoomBanner.svg" alt="banner" width={768} height={200} className="mb-3" />
+        <div className="flex flex-col max-h-[600px] bg-white shadow-lg rounded-lg overflow-hidden">
           <div className="flex-grow overflow-y-auto p-4">
             <ChatMessageList roomId={roomId} />
           </div>
