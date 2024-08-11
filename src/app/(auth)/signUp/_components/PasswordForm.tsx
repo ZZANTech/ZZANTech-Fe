f42704970187c5bf3416cd57674f231a6e1da753
joymet33 <joymet33@gmail.com>
2024-08-11 22:47:53 +0900

    fix: password form 유효성 검사 수정(영어+숫자+특수문자 조합 필수)

--- src/app/(auth)/signUp/_components/PasswordForm.tsx
+++ src/app/(auth)/signUp/_components/PasswordForm.tsx
@@ -43,11 +43,12 @@ function PasswordForm({
         type="password"
         value={password}
         maxLength={20}
-        placeholder="최소 6~20자, 영어+특수문자 조합"
+        placeholder="최소 6~20자, 영어+숫자+특수문자 조합"
         className={`auth-input ${passwordError ? "border-info-red" : isCorrected ? "border-info-green" : ""}`}
         onChange={handlePasswordChange}
       />
-      {passwordError && <p className="text-info-red text-xs">{passwordError}</p>}
+      <p className="text-info-red text-xs">{passwordError}</p>
+      <p className="text-info-green text-xs">{passwordError ? "" : isCorrected ? "사용 가능합니다" : ""}</p>
     </div>
   );
 }
--- src/utils/authValidity.ts
+++ src/utils/authValidity.ts
@@ -31,14 +31,18 @@ export const checkPasswordValidity = ({
     setPasswordError("비밀번호를 입력해주세요.");
     return;
   }
-  if (!/^[a-zA-Z\d!@#$%^&*()_+~`|}{[\]:;?><,./-=]{6,20}$/.test(password)) {
-    setPasswordError("영어, 특수문자 조합 필수");
-  }
-  if (password.length < 5 || password.length > 20) {
+  if (password.length < 6 || password.length > 20) {
     setPasswordError("비밀번호는 6자 이상 20자 이하이어야 합니다.");
     return;
   }
 
+  const hasLetter = /[a-zA-Z]/.test(password);
+  const hasDigit = /\d/.test(password);
+  const hasSpecialChar = /[~!@#$%^&*]/.test(password);
+  if (!(hasLetter && hasDigit && hasSpecialChar)) {
+    setPasswordError("영어, 숫자, 특수문자(~!@#$%^&*)를 모두 포함해야 합니다.");
+    return;
+  }
   setPasswordError("");
 };
 
@@ -54,12 +58,12 @@ export const checkNicknameValidity = ({
     setNicknameError("빈칸을 채워주세요.");
     return;
   }
-  if (/[^a-zA-Z0-9가-힣]/.test(nickname)) {
-    setNicknameError("특수문자를 포함하거나 한글 자음/모음 단독 사용은 어렵습니다.");
-    return;
-  }
   if (nickname.length < 2 || nickname.length > 8) {
     setNicknameError("닉네임은 2자 이상 7자 이하이어야 합니다.");
+    return;
+  }
+  if (/[^a-zA-Z0-9가-힣]/.test(nickname)) {
+    setNicknameError("특수문자를 포함하거나 한글 자음/모음 단독 사용은 어렵습니다.");
     return;
   }
   setNicknameError("");
