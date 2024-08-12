43a1b9e2766a9a30d79b7cbb44fd626ab0f5682d
joymet33 <joymet33@gmail.com>
2024-08-12 21:30:44 +0900

    remove: 컴포넌트에서 에러가 생겨 삭제

--- src/app/(auth)/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import LogInContainer from "@/app/(auth)/login/_components/LoginContainer";
-import React from "react";
-
-function login() {
-  return <LogInContainer />;
-}
-
-export default login;
--- src/app/(auth)/signUp/page.tsx
+++
@@ -1,8 +0,0 @@
-import SignUpContainer from "@/app/(auth)/signup/_components/SignUpContainer";
-import React from "react";
-
-function singup() {
-  return <SignUpContainer />;
-}
-
-export default singup;
