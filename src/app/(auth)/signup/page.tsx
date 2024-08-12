d1ca4b464be1043009fc4e66436d2ed6a6f4e021
joymet33 <joymet33@gmail.com>
2024-08-12 21:38:39 +0900

    chore: 로그인, 회원가입 폴더 및 파일 재생성

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+import SignUpContainer from "@/app/(auth)/signup/_components/SignUpContainer";
+import React from "react";
+
+function SignUp() {
+  return <SignUpContainer />;
+}
+
+export default SignUp;
