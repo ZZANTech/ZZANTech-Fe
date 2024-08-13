e2e67125d6e524bb280288fff2336f46807c208f
NHJeans <guswo1013@naver.com>
2024-08-13 10:21:24 +0900

    feat: 메인페이지 top-banner swiper 적용

--- src/app/(main)/_components/TopSection/TopSection.tsx
+++ src/app/(main)/_components/TopSection/TopSection.tsx
@@ -1,16 +1,11 @@
 import QuizUi from "@/app/(main)/_components/TopSection/QuizUi";
-import Image from "next/image";
-import Link from "next/link";
+import TopBanner from "@/app/(main)/_components/TopSection/TopBanner";
 
 function TopSection() {
   return (
     <section className="flex flex-col lg:flex-row gap-6 lg:gap-14">
-      <div className="lg:w-[700px]">
-        <Link href="/exchange">
-          <Image src="/home/home_banner.png" alt="home banner" width={700} height={250} className="w-full" />
-        </Link>
-      </div>
-      <div className="flex-grow flex items-center justify-center">
+      <TopBanner />
+      <div className="flex-grow flex items-center justify-center lg:justify-end">
         <QuizUi />
       </div>
     </section>
