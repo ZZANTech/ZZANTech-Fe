9d222ca6a42e4d48d39299feffd8479c67b37dd0
NHJeans <guswo1013@naver.com>
2024-08-14 18:11:24 +0900

    design: 메인페이지 top banner부분 1차 작업

--- src/app/(main)/_components/TopSection/TopBanner.tsx
+++ src/app/(main)/_components/TopSection/TopBanner.tsx
@@ -10,27 +10,41 @@ import Image from "next/image";
 
 function TopBanner() {
   return (
-    <div className=" lg:w-[700px] z-0">
+    <div className="lg:w-[700px] w-full h-[260px] lg:h-[250px] z-0">
       <Swiper
         pagination={{
           clickable: true,
           bulletClass: "swiper-pagination-bullet",
           bulletActiveClass: "swiper-pagination-bullet-active"
         }}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false
-        }}
+        // autoplay={{
+        //   delay: 2000,
+        //   disableOnInteraction: false
+        // }}
         modules={[Pagination, Autoplay]}
-        className="mySwiper py-20 h-full rounded-xl lg:py-0"
+        className="mySwiper h-full rounded-xl lg:py-0"
+        style={{ height: "100%" }}
       >
-        <SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center h-full relative">
           <Link href="/exchange">
-            <Image src="/home/home_banner.png" alt="home banner" width={700} height={250} className="w-full" />
+            <Image
+              src="/home/home_banner.svg"
+              alt="banner"
+              fill
+              className="object-cover hidden lg:block"
+              loading="eager"
+            />
+          </Link>
+          <Link href="/exchange">
+            <Image src="/home/banner_mobile.svg" alt="banner" fill className="object-cover lg:hidden" loading="eager" />
           </Link>
         </SwiperSlide>
-        <SwiperSlide className="bg-orange-500"></SwiperSlide>
-        <SwiperSlide className="bg-blue-300"></SwiperSlide>
+        <SwiperSlide className="bg-orange-500 flex items-center justify-center h-full">
+          <div className="text-white text-xl h-full flex items-center justify-center">Slide 2</div>
+        </SwiperSlide>
+        <SwiperSlide className="bg-blue-300 flex items-center justify-center h-full">
+          <div className="text-white text-xl h-full flex items-center justify-center">Slide 3</div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
