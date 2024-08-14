6ed464a921cd1e4808e7dee3a7925cc9672e3255
mangmuse <mangmangmuse@gmail.com>
2024-08-14 09:10:56 +0900

    refactor: extract pagination logic into usePagination custom hook

--- src/app/(main)/mypage/like/_components/LikedKnowhowContainer.tsx
+++ src/app/(main)/mypage/like/_components/LikedKnowhowContainer.tsx
@@ -6,40 +6,26 @@ import { ITEMS_PER_PAGE } from "@/app/(main)/boards/knowhow/_constants";
 import NoPostsMessage from "@/app/(main)/mypage/posts/_components/NoPostsMessage";
 import { useUserContext } from "@/provider/contexts/UserContext";
 import useLikedKnowhowsQuery from "@/stores/queries/knowhow/post/useLikedKnowhowsQuery";
-import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
+import usePagination from "@/hooks/usePagination";
 
 function LikedKnowhowContainer() {
   const { user } = useUserContext();
   const userId = user?.userId;
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const searchParams = useSearchParams();
-
+  const { currentPage, handlePageChange } = usePagination();
   const { data: knowhows, isPending } = useLikedKnowhowsQuery(currentPage, ITEMS_PER_PAGE, userId || "");
-
-  const handlePageChange = (page: number) => setCurrentPage(page);
-
-  useEffect(() => {
-    const pageFromParams = parseInt(searchParams.get("page") || "1", 10);
-    if (currentPage !== pageFromParams) {
-      setCurrentPage(pageFromParams);
-    }
-  }, [searchParams, currentPage]);
-
   const totalItems = knowhows ? knowhows[0]?.total_count || 0 : 0;
-  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
 
   return (
     <section>
       <h1 className="my-[63px] ml-[11px] text-[28px] font-semibold leading-9">좋아요 누른 글</h1>
-
       {isPending ? (
         <SkeletonKnowhowList />
       ) : knowhows && knowhows.length > 0 ? (
         <>
           <Suspense>
             <KnowhowList knowhows={knowhows} />
-            {totalPages > 1 && (
+            {totalItems > ITEMS_PER_PAGE && (
               <Pagination totalItems={totalItems} itemsPerPage={ITEMS_PER_PAGE} onPageChange={handlePageChange} />
             )}
           </Suspense>
--- src/app/(main)/mypage/posts/_components/MyVotesContainer.tsx
+++ src/app/(main)/mypage/posts/_components/MyVotesContainer.tsx
@@ -6,31 +6,19 @@ import VotesList from "@/app/(main)/boards/votes/_components/VotesList";
 import NoPostsMessage from "@/app/(main)/mypage/posts/_components/NoPostsMessage";
 import useMyVotesQuery from "@/stores/queries/vote/post/useMyVotesQuery";
 import { Tables } from "@/types/supabase";
-import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
+import usePagination from "@/hooks/usePagination";
 
 type MyVotesContainerProps = {
   user: Tables<"users">;
 };
-const ITEMS_PER_PAGE = 8;
-function MyVotesContainer({ user }: MyVotesContainerProps) {
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const searchParams = useSearchParams();
 
+const ITEMS_PER_PAGE = 8;
+
+function MyVotesContainer({ user }: MyVotesContainerProps) {
+  const { currentPage, handlePageChange } = usePagination();
   const { data: votes, isPending } = useMyVotesQuery(currentPage, ITEMS_PER_PAGE, user?.userId);
   const totalItems = votes?.[0]?.total_count ?? 0;
-  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
-  useEffect(() => {
-    const pageFromParams = parseInt(searchParams.get("page") || "1", 10);
-    if (currentPage !== pageFromParams) {
-      setCurrentPage(pageFromParams);
-    }
-  }, [searchParams]);
 
   return (
     <article>
@@ -39,7 +27,7 @@ function MyVotesContainer({ user }: MyVotesContainerProps) {
       ) : votes && votes.length > 0 ? (
         <>
           <VotesList votes={votes} />
-          {totalPages > 1 && (
+          {totalItems > ITEMS_PER_PAGE && (
             <Suspense>
               <Pagination itemsPerPage={ITEMS_PER_PAGE} totalItems={totalItems} onPageChange={handlePageChange} />
             </Suspense>
