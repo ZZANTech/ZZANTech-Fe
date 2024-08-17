15bddf1ad98ee9b4e70259f44e9784d6eb24243b
NHJeans <guswo1013@naver.com>
2024-08-17 22:52:02 +0900

    feat: 폰트최적화 재작업 완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,20 +6,20 @@ import Providers from "@/provider/Providers";
 import HeaderContainer from "@/app/(main)/_components/Header/HeaderContainer";
 import ConditionalFooter from "@/app/(main)/_components/Footer/ConditionalFooter";
 
-const pretendard = localFont({
-  src: [
-    {
-      path: "./assets/fonts/PretendardVariable.woff2",
-      weight: "45 920"
-    }
-    // {
-    //   path: "./assets/fonts/PretendardVariable.ttf",
-    //   weight: "45 920"
-    // }
-  ],
-  display: "swap",
-  variable: "--font-pretendard"
-});
+// const pretendard = localFont({
+//   src: [
+//     {
+//       path: "./assets/fonts/PretendardVariable.woff2",
+//       weight: "45 920"
+//     }
+//     // {
+//     //   path: "./assets/fonts/PretendardVariable.ttf",
+//     //   weight: "45 920"
+//     // }
+//   ],
+//   display: "swap",
+//   variable: "--font-pretendard"
+// });
 
 export const metadata: Metadata = {
   title: "ZZAN",
@@ -28,12 +28,42 @@ export const metadata: Metadata = {
     icon: "/favicon.png"
   }
 };
-
+// ${pretendard.variable} font-pretendard
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <link rel="icon" href="/favicon.png" sizes="any" />
-      <body className={`${pretendard.variable} font-pretendard flex flex-col min-h-screen`}>
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+        <link
+          rel="preload"
+          href="/fonts/subset-Pretendard-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/subset-Pretendard-SemiBold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/subset-Pretendard-Bold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/subset-Pretendard-ExtraBold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body className={`flex flex-col min-h-screen`}>
         <Providers>
           <div className="flex-grow max-w-[1120px] w-full mx-auto px-5 lg:px-0">
             <HeaderContainer />
