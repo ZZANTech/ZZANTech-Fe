c8a5f3e78c4439d50eb5e1c1ecd95994d5969c6a
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-08-17 18:27:03 +0900

    feat: vote write form mobile ui

--- src/app/(main)/boards/votes/edit/[voteId]/page.tsx
+++ src/app/(main)/boards/votes/edit/[voteId]/page.tsx
@@ -1,10 +1,16 @@
 import { getVote } from "@/apis/votes";
+import MobileHeader from "@/components/MobileHeader";
 import VoteWriteForm from "@/app/(main)/boards/votes/_components/VoteWriteForm";
 
 async function VoteEditPage({ params: { voteId } }: { params: { voteId: number } }) {
   const vote = await getVote(voteId);
 
-  return <VoteWriteForm previousContent={vote} />;
+  return (
+    <>
+      <MobileHeader title="글쓰기" />
+      <VoteWriteForm previousContent={vote} />
+    </>
+  );
 }
 
 export default VoteEditPage;
