939b6b14a7810209d245d959eef1745a3ca3469d
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-08-17 17:53:11 +0900

    feat: add mobile header to vote detail

--- src/app/(main)/boards/votes/[voteId]/page.tsx
+++ src/app/(main)/boards/votes/[voteId]/page.tsx
@@ -2,6 +2,7 @@ import { getVote, getVoteLikesData } from "@/apis/votes";
 import VoteContent from "@/app/(main)/boards/votes/[voteId]/_components/VoteContent";
 import CommentsContainer from "@/app/(main)/boards/_components/Comments/CommentsContainer";
 import NavButton from "@/app/(main)/boards/votes/[voteId]/_components/NavButton";
+import MobileHeader from "@/components/MobileHeader";
 import { Metadata } from "next";
 import { cookies } from "next/headers";
 
@@ -30,13 +31,16 @@ export default async function VoteDetailPage({ params: { voteId } }: VoteDetailP
   const [vote, voteLikes] = await Promise.all([getVote(voteId), getVoteLikesData(voteId, accessToken, refreshToken)]);
 
   return (
-    <div className="flex flex-col justify-center items-center gap-[23px] mt-3 md:mt-[77px] w-full">
-      <section className="flex items-center w-full max-w-[904px]">
-        <NavButton vote={vote} direction="prev" />
-        <VoteContent vote={vote} initialVoteLikes={voteLikes} accessToken={accessToken} refreshToken={refreshToken} />
-        <NavButton vote={vote} direction="next" />
-      </section>
-      <CommentsContainer postId={voteId} board="vote" />
-    </div>
+    <>
+      <MobileHeader title="짠 소비 구경" />
+      <div className="flex flex-col justify-center items-center gap-[23px] mt-3 md:mt-[77px] w-full">
+        <section className="flex items-center w-full max-w-[904px]">
+          <NavButton vote={vote} direction="prev" />
+          <VoteContent vote={vote} initialVoteLikes={voteLikes} accessToken={accessToken} refreshToken={refreshToken} />
+          <NavButton vote={vote} direction="next" />
+        </section>
+        <CommentsContainer postId={voteId} board="vote" />
+      </div>
+    </>
   );
 }
