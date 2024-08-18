e701757374c226eae7ac5025f5a0681a8745e5f8
mangmuse <mangmangmuse@gmail.com>
2024-08-18 20:40:18 +0900

    feat: claim list pagination

--- src/stores/queries/exchange/useClaimsQuery.ts
+++ src/stores/queries/exchange/useClaimsQuery.ts
@@ -1,11 +1,12 @@
 import { getClaims } from "@/apis/exchange";
+import { TClaimsResponse } from "@/types/exchange.type";
 import { Tables } from "@/types/supabase";
 import { useQuery } from "@tanstack/react-query";
 
-const useClaimsQuery = (userId: string) => {
-  return useQuery({
+const useClaimsQuery = (currentPage: number, itemsPerPage: number, userId: string) => {
+  return useQuery<TClaimsResponse>({
     queryKey: ["claims", { userId }],
-    queryFn: () => getClaims(userId)
+    queryFn: () => getClaims(currentPage, itemsPerPage, userId)
   });
 };
 
--- src/types/exchange.type.ts
+++ src/types/exchange.type.ts
@@ -2,3 +2,8 @@ import { Tables } from "@/types/supabase";
 export type TClaim = Tables<"gift_claims"> & {
   gift_name: string;
 };
+
+export type TClaimsResponse = {
+  data: TClaim[];
+  totalCount: number;
+};
